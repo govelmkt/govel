dfdd686965bf7a988f1eb3b8edde98ae04a7e594
Manu <manuvaglz@gmail.com>
2023-03-25 11:14:54 -0600

    feat: Add privacy policy page

---
+++ app/privacy-policy/page.jsx
@@ -0,0 +1,26 @@
+const PrivacyPolicy = () => {
+  return (
+    <section className='mt-24 max-w-2xl m-auto p-6'>
+      <h1 className='text-4xl text-center my-6'>Aviso de Privacidad</h1>
+      <p>De acuerdo a lo Previsto en la “Ley Federal de Protección de Datos Personales”, declara <strong>GOVEL SA DE CV y/o Francisco Gómez Velázquez  (Representante Legal)</strong>, ser una empresa moral legalmente constituida de conformidad con las leyes mexicanas, con domicilio fiscal en <strong>Mayas #760 en Tepatitlán de Morelos, Jalisco</strong>, y como responsable del tratamiento de sus datos personales, hace de su conocimiento que la información de nuestros clientes es tratada de forma estrictamente confidencial por lo que al proporcionar sus datos personales, <strong>tales como:</strong></p>
+      <ul className='list-decimal  font-bold p-6'>
+        <li>Nombre y/o Razón social</li>
+        <li>Teléfonos de Oficina y móviles</li>
+        <li>Correo Electrónico</li>
+      </ul>
+      <p className=''>Estos serán utilizados única y exclusivamente para los siguientes fines:</p>
+      <ul className='list-decimal font-bold p-6'>
+        <li>Campañas de Publicidad</li>
+        <li>Campañas de Fidelidad.</li>
+        <li>Información y Prestación de Servicios.</li>
+        <li>Actualización de la Base de Datos.</li>
+        <li>Cualquier finalidad análoga o compatible con las anteriores.</li>
+      </ul>
+      <p>Para prevenir el acceso no autorizado a sus datos personales y con el fin de asegurar que la información sea utilizada para los fines establecidos en este aviso de privacidad, hemos establecido diversos procedimientos con la finalidad de evitar el uso o divulgación no autorizados de sus datos, permitiéndonos tratarlos debidamente. Así mismo, le informamos que sus datos personales pueden ser transmitidos para ser tratados por personas distintas a esta empresa, pero siempre para los fines establecidos en éste aviso y bajo supervisión de <strong>GOVEL SA DE CV.</strong> Todos sus datos personales son tratados de acuerdo a la legislación aplicable y vigente en el país, por ello le informamos que usted tiene en todo momento los derechos de acceder, rectificar, cancelar u oponerse al tratamiento que le damos a sus datos personales; derecho que podrá hacer valer a través del <strong>Área de Privacidad encargada de la seguridad de datos personales en el Teléfono 378 121 7912 o al correo electrónico  arq.fco.gomez@hotmail.com.</strong>
+      </p>
+    </section>
+
+  )
+}
+
+export default PrivacyPolicy
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export const Footer = () => {
   return (
     <footer className='flex flex-col items-center justify-center p-6'>
@@ -5,6 +7,7 @@ export const Footer = () => {
       <p>Mayas #760</p>
       <p>Las colonias</p>
       <p>Tepatitlán de Morelos, Jalisco</p>
+      <Link href='/privacy-policy'><small>Aviso de Privacidad</small></Link>
     </footer>
   )
 }
