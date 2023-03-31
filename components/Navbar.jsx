488146af19fff4f8d70a7eb2ee363747579d3e0b
Manu <manuvaglz@gmail.com>
2023-03-30 20:34:33 -0600

    Update icons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { AiOutlineInstagram, AiOutlineWhatsApp } from 'react-icons/ai'
+import { AiOutlineInstagram, AiOutlineWhatsApp, AiOutlineFacebook } from 'react-icons/ai'
 
 export const Navbar = () => {
   return (
@@ -10,8 +10,9 @@ export const Navbar = () => {
         <ul className='flex gap-3 items-center uppercase'>
           <li><Link href='/'>Home</Link></li>
           <li><Link href='/projects'>Proyectos</Link></li>
-          <li><Link href='https://instagram.com'><AiOutlineInstagram size='1.3rem' /></Link></li>
-          <li><Link href='/'><AiOutlineWhatsApp size='1.3rem' /></Link></li>
+          <li><Link href='https://www.instagram.com/govelproyectosarquitectonicos/'><AiOutlineInstagram size='1.3rem' /></Link></li>
+          <li><Link href='https://www.facebook.com/govelproyectosarquitectonicos/'><AiOutlineFacebook size='1.3rem' /></Link></li>
+          <li><Link href='https://api.whatsapp.com/send?phone=5213781217917&text=Hola%2C%20estoy%20en%20tu%20p%C3%A1gina%20web%20www.govel.com.mx%20y%20quiero%20informaci%C3%B3n...'><AiOutlineWhatsApp size='1.3rem' /></Link></li>
         </ul>
       </nav>
     </header>
